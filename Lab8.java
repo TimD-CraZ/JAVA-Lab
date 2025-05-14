1dd5bcb45c53185af4c53399ba4a7e9c13ca381b
TimD-CraZ <tanmaykrishnav@gmail.com>
2025-05-14 12:12:42 +0530

    Add files via upload

--- Lab8.java
+++ Lab8.java
@@ -1,5 +1,37 @@
-package Tanmay;
+class LessBalanceExecption extends Exception {
+    public LessBalanceExecption(double amount) {
+        super("Withdraw amount (" + amount + "Rs) is not possible.");
+    }
+}
+
+class Amount {
+    double balance;
+    static final double MIN_BALANCE = 500;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws LessBalanceExecption {
+        if (amount > this.balance || (balance - amount) < MIN_BALANCE) {
+            throw new LessBalanceExecption(amount);
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
 
 public class Lab8 {
-    
+    public static void main(String[] args) throws LessBalanceExecption {
+        Amount obj1 = new Amount();
+
+        obj1.deposit(1000);
+        System.out.println("Current Balance: " + obj1.getBalance());
+        obj1.withdraw(500);
+        System.out.println("Balance after withdrawing: " + obj1.getBalance());
+    }
 }
